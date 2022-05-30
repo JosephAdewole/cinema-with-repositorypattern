d808e5eb9055c46a0ad4d357f7af4213b059a036
JosephAdewole <josephadewole1@gmail.com>
2022-05-30 10:17:46 +0100

    set up mvc for cinemas and movies

---
+++ app/Http/Controllers/CinemaController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cinema;
+use Illuminate\Http\Request;
+
+class CinemaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cinema  $cinema
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cinema $cinema)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cinema  $cinema
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cinema $cinema)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cinema  $cinema
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cinema $cinema)
+    {
+        //
+    }
+}
